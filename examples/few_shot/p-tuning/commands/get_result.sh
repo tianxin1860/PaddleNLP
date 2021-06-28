b69f180ef9581bb4b5b02ecbbc7e8d427c026699
tianxin <tianxin04@baidu.com>
2021-06-28 16:39:53 +0800

    add script

---
+++ examples/few_shot/p-tuning/commands/get_result.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+#task_names=(iflytek tnews eprstmt bustm ocnli csl)
+#all_gpu=(0 1 2 3 4 5)
+
+#task_name="cluewsc"
+#gpus=0
+
+#task_name="tnews"
+#gpus=1
+
+#task_name="eprstmt"
+#gpus=2
+
+#task_name="chid"
+#gpus=3
+
+#task_name="bustm"
+#gpus=4
+
+#task_name="ocnli"
+#gpus=5
+
+
+#task_name="csldcp"
+#gpus=7
+
+#task_name="iflytek"
+#gpus=5
+
+#task_name="csl"
+#gpus=7
+
+#export pretrained_model="ernie1p0"
+#export pretrained_model="macbert-base-chinese"
+export pretrained_model="macbert-large-chinese"
+task_name=$1
+gpus=$2
+
+#indexs=(0 1 2 3 4 few_all)
+indexs=(0)
+#indexs=(3)
+#indexs=(4)
+#indexs=(few_all)
+#indexs=(0 few_all)
+
+for index in ${indexs[@]}; do
+	if [[ ${pretrained_model} == "ernie1p0" ]]; then
+		bash commands/train.sh ${task_name} ${gpus} ${index} > gpu${gpus}_train_${pretrained_model}_${task_name}_index${index}.log 2>&1
+	else
+		bash commands/train_macbert.sh ${task_name} ${gpus} ${index} > get_result_${pretrained_model}_${task_name}_index${index}.log 2>&1
+	fi
+done
---
+++ examples/few_shot/p-tuning/get_final.py
@@ -0,0 +1,61 @@
+import os
+import sys
+import numpy as np
+
+from shutil import copyfile
+
+if __name__=="__main__":
+    result_file = sys.argv[1]
+    output_dir = sys.argv[2]
+    task_name = sys.argv[3]
+    index = sys.argv[4]
+    submit_result_dir = sys.argv[5]
+
+    if not os.path.exists(submit_result_dir):
+        os.mkdir(submit_result_dir)
+
+    dev_accs = []
+    #test_accs = []
+    strategys = []
+    epochs = []
+
+    dev_num = 0
+    #test_num = 0
+
+    with open(result_file) as f:
+        for line in f:
+            strategy_name, epoch, dev_acc, dev_num = line.strip().split("\t")
+
+            strategys.append(strategy_name)
+            dev_accs.append(float(dev_acc))
+            #test_accs.append(float(test_acc))
+            epochs.append(epoch)
+
+    print("dev_accs:{}".format(dev_accs))
+    #print("test_accs:{}".format(test_accs))
+
+    max_index = np.argmax(dev_accs)
+    max_dev_acc = dev_accs[max_index]
+    #test_acc = test_accs[max_index]
+
+    strategy = strategys[max_index]
+    epoch = epochs[max_index]
+
+    index_name = index if index != "few_all" else "all"
+
+    if task_name not in ["eprstmt", "csldcp", "bustm"]:
+        best_predict_file = "index" + index + "_" + epoch  + "epoch_" + task_name + "f_predict.json"
+        std_name = task_name + "f_predict_" + index_name + ".json"
+    else:
+        best_predict_file = "index" + index + "_" + epoch  + "epoch_" + task_name + "_predict.json"
+        std_name = task_name + "_predict_" + index_name + ".json"
+
+    predict_file = os.path.join(output_dir, strategy, task_name, best_predict_file)
+    print("best_result_file:{}".format(predict_file))
+
+    submit_file = os.path.join(submit_result_dir, std_name)
+    print("std_result_file:{}".format(submit_file))
+
+    print("{}\t{}\t{}\t{}".format(strategy, epoch, max_dev_acc, dev_num))
+
+    copyfile(predict_file, submit_file)
