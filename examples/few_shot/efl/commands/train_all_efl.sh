ad9f689251ff74523a1e0c647638c584c5a79f09
tianxin <tianxin04@baidu.com>
2021-09-14 11:08:06 +0800

    add train_all_efl.sh

---
+++ examples/few_shot/efl/commands/train_all_efl.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+#task_names=(iflytek tnews eprstmt bustm ocnli csl)
+#all_gpu=(0 1 2 3 4 5)
+
+#task_name="cluewsc"
+#gpus=0
+
+#task_name="tnews"
+#gpus=1
+
+#task_name="eprstmt"
+#gpus=2
+
+#task_name="chid"
+#gpus=3
+
+#task_name="bustm"
+#gpus=4
+
+#task_name="ocnli"
+#gpus=5
+
+
+#task_name="csldcp"
+#gpus=7
+
+#task_name="iflytek"
+#gpus=5
+
+#task_name="csl"
+#gpus=7
+
+export pretrained_model="ernie1p0"
+export name="fewclue_paper"
+
+task_name=$1
+gpus=$2
+
+#indexs=(0 1 2 3 4 few_all)
+indexs=(2 3 4 few_all)
+#indexs=(3 4 few_all)
+
+for index in ${indexs[@]}; do
+        bash commands/train_efl.sh ${task_name} ${gpus} ${index} > ${name}_gpu${gpus}_train_${pretrained_model}_${task_name}_index${index}.log 2>&1
+done
